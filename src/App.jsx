b3100cf6b70835000242271da85804c609d13068
KEN-4 <cuuw1561@icloud.com>
2023-12-02 18:21:39 +0900

    clear station3

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,8 @@ export const App = () => {
     <div>
       <header>
         <h1>Dogアプリ</h1>
+        <p> 犬の画像を表示するサイトです</p>
+        <img src='https://images.dog.ceo/breeds/spaniel-brittany/n02101388_6057.jpg'></img>
       </header>
     </div>
   )

a673d2c77c367020db13c322d82e5b79484a4bbe
KEN-4 <cuuw1561@icloud.com>
2023-12-03 13:04:09 +0900

    clear station6

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,16 @@ import './App.css'
  * @type {() => JSX.Element}
  */
 export const App = () => {
-  const [dogUrl, setDogUrl] = useState('https://images.dog.ceo/breeds/spaniel-brittany/n02101388_6057.jpg')
+  const [dogUrl, setDogUrl] = useState('https://images.dog.ceo/breeds/spaniel-brittany/n02101388_6057.jpg');
+  const changeImage = () => {
+    fetch("https://dog.ceo/api/breeds/image/random")
+      .then(res => res.json())
+      .then((data) => {setDogUrl(data.message);
+      })
+      .catch((error) => {
+        console.error("画像の取得に失敗しました", error);
+      });
+    };
 
   return (
     <div>
@@ -15,6 +24,7 @@ export const App = () => {
         <h1>Dogアプリ</h1>
         <p> 犬の画像を表示するサイトです</p>
         <img src={dogUrl} alt='犬の画像'></img>
+        <button onClick={changeImage}>更新</button>
       </header>
     </div>
   )

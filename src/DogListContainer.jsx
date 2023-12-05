ae08bbd7501b2250d62c9e195bc2fe4b631f7f53
KEN-4 <cuuw1561@icloud.com>
2023-12-05 19:53:55 +0900

    clear station9

--- src/DogListContainer.jsx
+++ src/DogListContainer.jsx
@@ -1,7 +1,18 @@
 // @ts-check
+import React, { useState, useEffect } from 'react';
 
 export const DogListContainer = () => {
-  return <></>
+
+const [breeds, setBreeds] = useState([]);
+
+useEffect(() => {
+  fetch("https://dog.ceo/api/breeds/list/all")
+    .then((res) => res.json())
+    .then((data) => {setBreeds(data.message);
+    })
+    .catch((error) => {
+      console.error("犬種一覧の取得に失敗しました", error);
+    });
+  }, []); 
 }
 
-export default DogListContainer

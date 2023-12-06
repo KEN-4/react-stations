fb47116134126ad58d765f8f437a0e244198cb89
KEN-4 <cuuw1561@icloud.com>
2023-12-06 18:43:49 +0900

    clear station11

--- src/BreedsSelect.jsx
+++ src/BreedsSelect.jsx
@@ -1,7 +1,17 @@
 // @ts-check
+import React from 'react';
 
-export const BreedsSelect = () => {
-  return <></>
-}
+export const BreedsSelect = ({ breeds, selectedBreed, onBreedChange }) => {
+  return (
+    <label>
+      Breeds List
+      <select value={selectedBreed} onChange={(e) => onBreedChange(e.target.value)}>
+        {breeds.map(breed => (
+          <option key={breed} value={breed}>{breed}</option>
+        ))}
+      </select>
+    </label>
+  );
+};
 
-export default BreedsSelect
+export default BreedsSelect;
--- src/DogListContainer.jsx
+++ src/DogListContainer.jsx
@@ -1,18 +1,46 @@
 // @ts-check
 import React, { useState, useEffect } from 'react';
+import BreedsSelect from './BreedsSelect';
 
 export const DogListContainer = () => {
-
 const [breeds, setBreeds] = useState([]);
+const [selectedBreed, setSelectedBreed] = useState('');
+const [breedImages, setBreedImages] = useState([]);
 
 useEffect(() => {
   fetch("https://dog.ceo/api/breeds/list/all")
     .then((res) => res.json())
-    .then((data) => {setBreeds(data.message);
+    .then((data) => {setBreeds(Object.keys(data.message));
     })
     .catch((error) => {
       console.error("犬種一覧の取得に失敗しました", error);
     });
   }, []); 
+
+  const handleBreedChange = (breed) => {
+    setSelectedBreed(breed);
+  };
+
+  const handleShowImages = () => {
+    if (selectedBreed) {
+      fetch(`https://dog.ceo/api/breed/${selectedBreed}/images/random/12`)
+        .then((res) => res.json())
+        .then((data) => setBreedImages(data.message))
+        .catch((error) => console.error("犬種の画像リスト取得に失敗しました", error));
+    }
+  };
+
+  return (
+    <div>
+      <BreedsSelect breeds={breeds} selectedBreed={selectedBreed} onBreedChange={handleBreedChange} />
+      <button onClick={handleShowImages}>表示</button>
+      <div>
+        {breedImages.map((image, index) => (
+          <img key={index} src={image} alt={selectedBreed} />
+        ))}
+      </div>
+    </div>
+  );
 }
 
+export default DogListContainer;
